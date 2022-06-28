fdb7033fa40f5a606fa6586635ce31af02dac229
Nicolasios <1498913522@qq.com>
2022-06-28 13:52:13 +0800

    增分页查询机制

--- src/main/java/com/example/ty/controller/UserController.java
+++ src/main/java/com/example/ty/controller/UserController.java
@@ -1,7 +1,11 @@
 package com.example.ty.controller;
 
+import com.example.ty.common.PageResult;
+import com.example.ty.constant.Constant;
 import com.example.ty.entity.UserEntity;
 import com.example.ty.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 import java.util.List;
 import java.util.Map;
-
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
@@ -20,5 +24,16 @@ public class UserController {
     @GetMapping("/findAll")
     public List<UserEntity> findAll(@RequestParam Map<String,Object> paraMap){
         return userService.findAll(paraMap);
+    }
+
+    @GetMapping("/page/findAll")
+    public PageResult<UserEntity> findAllPage(@RequestParam("current") Integer pageNo,
+                                              @RequestParam("pageSize") Integer pageSize,
+                                              @RequestParam Map<String,Object> paraMap){
+        Page<UserEntity> data = userService.findAllPage(paraMap,pageNo-1,pageSize);
+        Long total = data.getTotalElements();
+        List<UserEntity> content = data.getContent();
+        log.info("content:{}",content);
+        return PageResult.ok(Constant.OK,"查询成功",pageNo,pageSize,total,content);
     }
 }
--- src/main/java/com/example/ty/service/UserService.java
+++ src/main/java/com/example/ty/service/UserService.java
@@ -3,6 +3,9 @@ package com.example.ty.service;
 import com.example.ty.entity.UserEntity;
 import com.example.ty.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
@@ -21,6 +24,26 @@ public class UserService {
     @Resource
     private UserRepository userRepository;
 
+    public Page<UserEntity> findAllPage(Map<String,Object> paramMap,int pageNum,int pageSize){
+        Sort sort = Sort.by("id").descending();
+        PageRequest pageable = PageRequest.of(pageNum, pageSize, sort);
+        Integer number = paramMap.get("number") == null ? 0 : Integer.parseInt((String) paramMap.get("number")) ;
+        String address = paramMap.get("address") == null ? "" : (String) paramMap.get("address");
+        Specification<UserEntity> spec = (root, query, criteriaBuilder) -> {
+            List<Predicate> predicates = new ArrayList<>();
+            Join<Object, Object> joinPart = root.join("partEntity", JoinType.INNER);
+            if (!StringUtils.isEmpty(address)) {
+                predicates.add(criteriaBuilder.like(joinPart.get("address"), "%" + address + "%"));
+            }
+            if(number!=0){
+                predicates.add(criteriaBuilder.equal(joinPart.get("number"),  number));
+            }
+            return query.where(predicates.toArray(new Predicate[predicates.size()])).getRestriction();
+        };
+        Page<UserEntity> all = userRepository.findAll(spec,pageable);
+        return all;
+    }
+
     public List<UserEntity> findAll(Map<String, Object> paramMap){
         log.info("paramMap:{}",paramMap);
         Integer number = paramMap.get("number") == null ? 0 : Integer.parseInt((String) paramMap.get("number")) ;
@@ -28,7 +51,6 @@ public class UserService {
         log.info("number:{}",number);
         log.info("address:{}",address);
         Specification<UserEntity> spec = (root, query, criteriaBuilder) -> {
-            log.info("root:{}",root);
             List<Predicate> predicates = new ArrayList<>();
             Join<Object, Object> joinPart = root.join("partEntity", JoinType.INNER);
             if (!StringUtils.isEmpty(address)) {
